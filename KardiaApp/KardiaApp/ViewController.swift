3556b57446e2c5b61e937cbaf78f41c54e2dcb85
cdepman <cdepman@gmail.com>
2014-12-30 16:41:38 -0800

    adds BLE connection status indicator and LaunchScreen logo

--- KardiaApp/KardiaApp/ViewController.swift
+++ KardiaApp/KardiaApp/ViewController.swift
@@ -8,12 +8,42 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var BLEDisconnected: UIImageView!
+    
+    @IBOutlet weak var BLEConnected: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("connectionChanged:"), name: BLEServiceChangedStatusNotification, object: nil)
+        
         // Start the Bluetooth discovery process
         btDiscoverySharedInstance
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return false
+    }   
+    
+    func connectionChanged(notification: NSNotification) {
+        // Connection status changed. Indicate on GUI.
+        let userInfo = notification.userInfo as [String: Bool]
+        
+        dispatch_async(dispatch_get_main_queue(), {
+            // Set image based on connection status
+            if let isConnected: Bool = userInfo["isConnected"] {
+                if isConnected {
+                    self.BLEDisconnected.image = UIImage(named: "Bluetooth_Connected")
+                } else {
+                    self.BLEConnected.image = UIImage(named: "Bluetooth_Disconnected")
+                }
+            }
+        });
     }
     
 }
