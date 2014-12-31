badf9cce5b2792bdbef587f4822035530c00b87f
Bernard Chu <bernard.chu@yahoo.com>
2014-12-31 11:27:29 -0800

    Graph now draws with each incoming data point but doesn't properly erase old lines.

--- KardiaApp/KardiaApp/ViewController.swift
+++ KardiaApp/KardiaApp/ViewController.swift
@@ -5,10 +5,6 @@
 //  Created by Charlie Depman on 12/29/14.
 //  Copyright (c) 2014 Kardia. All rights reserved.
 //
-
-
-
-
 import UIKit
 
 class ViewController: UIViewController, LineChartDelegate {
@@ -20,29 +16,9 @@ class ViewController: UIViewController, LineChartDelegate {
     
     @IBOutlet weak var BLEConnected: UIImageView!
 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("connectionChanged:"), name: BLEServiceChangedStatusNotification, object: nil)
-        
-        // Start the Bluetooth discovery process
-        btDiscoverySharedInstance
-        
-
-        println(view)
-
-        label.text = "Incoming Data"
-        label.setTranslatesAutoresizingMaskIntoConstraints(false)
-        label.textAlignment = NSTextAlignment.Center
-        self.view.addSubview(label)
-        views["label"] = label
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: nil, metrics: nil, views: views))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-80-[label]", options: nil, metrics: nil, views: views))
-        
-        var data: Array<CGFloat> = [4.492, 5.120, 4.440, 4.917, 5.009]
-        
+    func makeChart(data: [CGFloat]){
         lineChart = LineChart()
+        lineChart!.clearAll()
         lineChart!.animationEnabled = false
         lineChart!.gridVisible = false
         lineChart!.dotsVisible = false
@@ -53,25 +29,34 @@ class ViewController: UIViewController, LineChartDelegate {
         views["chart"] = lineChart
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: nil, metrics: nil, views: views))
         view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: nil, metrics: nil, views: views))
+        view.setNeedsDisplay()
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-//        var delta: Int64 = 4 * Int64(NSEC_PER_SEC)
-//        var time = dispatch_time(DISPATCH_TIME_NOW, delta)
-//        
-//        dispatch_after(time, dispatch_get_main_queue(), {
-//            if let bleService = btDiscoverySharedInstance.bleService {
-//                self.lineChart!.clear()
-//                println("foo")
-//            }
-//        });
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("connectionChanged:"), name: BLEServiceChangedStatusNotification, object: nil)
+        
+        // Start the Bluetooth discovery process
+        btDiscoverySharedInstance
+
+        label.text = "Incoming Data"
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        label.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(label)
+        views["label"] = label
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: nil, metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-80-[label]", options: nil, metrics: nil, views: views))
         
         // Listen for incoming data from Bluetooth
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("drawGraph:"), name: GotBLEDataNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("processData:"), name: GotBLEDataNotification, object: nil)
     }
-
     
     override func shouldAutorotate() -> Bool {
         return false
-    }   
+    }
     
     func connectionChanged(notification: NSNotification) {
         // Connection status changed. Indicate on GUI.
@@ -93,29 +78,18 @@ class ViewController: UIViewController, LineChartDelegate {
 
     // Callback function called when ViewController learns of incoming data
 
-    func drawGraph(notification: NSNotification) {
+    func processData(notification: NSNotification) {
         // Data passed along needs to be type converted to an array of CGFloats in order to be used by lineChart
 
         let data = notification.userInfo!["passData"]! as [String]
         let cgFloatData = data.map {
             CGFloat(($0 as NSString).doubleValue)
         }
-        println(cgFloatData)
         
-        // Draw new graph
-        lineChart!.clear()
-        lineChart!.addLine(cgFloatData)
-        lineChart!.setTranslatesAutoresizingMaskIntoConstraints(false)
-        println("before")
-        println(views["chart"])
-        views["chart"] = lineChart
-        println("after")
-        println(views["chart"])
-        lineChart!.addLine(cgFloatData)
-        lineChart!.setTranslatesAutoresizingMaskIntoConstraints(false)
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: nil, metrics: nil, views: views))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: nil, metrics: nil, views: views))
-
+        // Draw graph with data
+        dispatch_async(dispatch_get_main_queue()) {
+            self.makeChart(cgFloatData)
+        }
     }
 
     
